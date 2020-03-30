c1fae133c147d40c6ecc710e9da8995a10777bd4
Adam Priest <sepone229@gmail.com>
2020-03-30 14:25:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,21 +1,16 @@
 class Song
-  
   attr_accessor :artist, :name
   @@all = []
-  
   def initialize(name)
     @name = name
     save
   end
-  
   def save
     @@all << self
   end
-  
   def self.all
     @@all
   end
-  
   def artist_name
     if @artist == nil
       return nil
